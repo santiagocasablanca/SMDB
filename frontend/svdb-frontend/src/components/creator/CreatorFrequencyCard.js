3c481b372517b3aee3c629df095813a61cbb7c7f
Tiago Martins Lomba <tiagomartinslomba@Tiagos-Air.lan>
2023-07-14 19:01:24 +0100

    fixed date filter bug for frequency cards

--- frontend/svdb-frontend/src/components/creator/CreatorFrequencyCard.js
+++ frontend/svdb-frontend/src/components/creator/CreatorFrequencyCard.js
@@ -360,14 +360,14 @@ const CreatorFrequencyCard = (_channels) => {
             <Option value="2018">2018</Option>
             <Option value="2017">2017</Option>
             <Option value="2016">2016</Option>
-            <Option value="2019">2015</Option>
-            <Option value="2018">2014</Option>
-            <Option value="2017">2013</Option>
-            <Option value="2016">2012</Option>
-            <Option value="2019">2011</Option>
-            <Option value="2018">2010</Option>
-            <Option value="2017">2009</Option>
-            <Option value="2016">2008</Option>
+            <Option value="2015">2015</Option>
+            <Option value="2014">2014</Option>
+            <Option value="2013">2013</Option>
+            <Option value="2012">2012</Option>
+            <Option value="2011">2011</Option>
+            <Option value="2010">2010</Option>
+            <Option value="2009">2009</Option>
+            <Option value="2008">2008</Option>
             {/* Add more options as needed */}
           </Select>
           
