c665ac962c62eb3c19328d653fb271bb6ea0c513
Tiago Martins Lomba <tiagomartinslomba@Tiagos-Air.lan>
2023-07-27 22:04:59 +0100

    running fetch

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,15 +17,15 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 // 11am
 cron.schedule('00 00 * * *', () => {
   console.log('schuduled and running');
-  const youtubeService = new YoutubeService();
-  youtubeService.fetchStatisticsForAllChannels();
+  // const youtubeService = new YoutubeService();
+  // youtubeService.fetchStatisticsForAllChannels();
 })
 
-cron.schedule('00 12 * * *', () => {
-  console.log('schuduled and running');
-  // const videoMetaService = new VideoMetaService();
-  // videoMetaService.associateTagsToVideos();
-})
+// cron.schedule('00 12 * * *', () => {
+//   console.log('schuduled and running');
+//   const videoMetaService = new VideoMetaService();
+//   videoMetaService.associateTagsToVideos();
+// })
 
 const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN ? process.env.CLIENT_ORIGIN : 'http://localhost:3000';
 app.use(
@@ -64,8 +64,8 @@ db.sequelize
     console.log("Database connected");
     app.listen(PORT, async () => {
       console.log("listening at port 8005");
-      // const youtubeService = new YoutubeService();
-      // youtubeService.fetchStatisticsForAllChannels();
+      const youtubeService = new YoutubeService();
+      youtubeService.fetchStatisticsForAllChannels();
       // youtubeService.updateAllCreatorPicturesFromMainChannel();
 
 
